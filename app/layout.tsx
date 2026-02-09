49c44c321d884bbfead41f150579841767662400
Abhishek Patil <itsmeabhishek65@gmail.com>
2026-02-09 17:28:56 +0530

    updated metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,36 +10,36 @@ import './globals.css'
 
 export const metadata = {
   title: {
-    default: 'KGG',
-    template: '%s | KGG',
+    default: 'AP',
+    template: '%s | AP',
   },
   authors: [
-    { name: 'Kumar Gaurav Govinda', url: 'https://www.linkedin.com/in/krgauravgovinda/' },
+    { name: 'Abhishek Patil', url: 'https://www.linkedin.com/in/abhishek-patil01/' },
   ],
-  creator: 'Kumar Gaurav Govinda',
+  creator: 'Abhihshek Patil',
   // icons: { icon: '' },
   openGraph: {
-    title: 'KGG',
+    title: 'AP',
     description:
-      'Kumar Gaurav Govinda DEV, website and web application development.',
+      'Abhishek Patil DEV, website and web application development.',
     type: 'website',
     locale: 'en_US',
-    url: 'https://gauravgovinda.vercel.app',
+    url: 'https://portfolio-craft-two.vercel.app/',
   },
-  applicationName: 'KGG',
+  applicationName: 'AP',
   description:
-    'Kumar Gaurav Govinda DEV, website and web application development.',
+    'Abhishek Patil DEV, website and web application development.',
   category: 'Web Development',
   keywords:
     'Web Development, Website Development, Application Development, Web Application Development, Website Development, Website Development in Ranchi, Application Development in Ranchi, Web Application Development in Ranchi, Website Development in Ranchi, Website Development in India, Application Development in India, Web Application Development in India, Web Site Development in India, Website Development in Ranchi India, Application Development in Ranchi India, Web Application Development in Ranchi India, Website Development in Ranchi India, Website Development in India, Application Development in India, Web Application Development in India, Website Development in India, Website Development in Ranchi India, Application Development in Ranchi India, Web Application Development in Ranchi India, Web Site Development in Ranchi India',
   classification: 'Web Development',
   robots: 'index, follow',
-  publisher: 'KGG',
+  publisher: 'AP',
   referrer: 'no-referrer-when-downgrade',
   alternates: {
-    canonical: 'https://gauravgovinda.vercel.app',
+    canonical: 'https://portfolio-craft-two.vercel.app/',
     languages: {
-      'en-US': 'https://gauravgovinda.vercel.app',
+      'en-US': 'https://portfolio-craft-two.vercel.app/',
     },
   },
 }
